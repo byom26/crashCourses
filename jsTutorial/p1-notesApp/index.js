efed253fc681c25b2c1868e1e4b77ec102e3f4f2
byom26 <p.byom26@gmail.com>
2021-01-10 17:10:37 +0530

    added notes title to magic notes

--- jsTutorial/p1-notesApp/index.js
+++ jsTutorial/p1-notesApp/index.js
@@ -3,20 +3,27 @@ showNotes();
 let addBtn = document.getElementById('addBtn');
 addBtn.addEventListener("click", function (e) {
     let addTxt = document.getElementById("addTxt");
+    let addTitle = document.getElementById("addTitle");
     let notes = localStorage.getItem("notes");
+    let titles = localStorage.getItem("titles");
 
-    if (addTxt.value != "") {
-        console.log(addTxt.value)
+    if (addTxt.value != "" && addTitle.value != "") {
+        //console.log(addTxt.value)
         if (notes == null) {
             notesObj = [];
+            titlesObj = [];
         }
         else {
             notesObj = JSON.parse(notes);
+            titlesObj = JSON.parse(titles);
         }
 
         notesObj.push(addTxt.value);
+        titlesObj.push(addTitle.value);
         localStorage.setItem("notes", JSON.stringify(notesObj));
+        localStorage.setItem("titles", JSON.stringify(titlesObj));
         addTxt.value = "";
+        addTitle.value = "";
         //console.log(notesObj);
 
         showNotes();
@@ -25,7 +32,7 @@ addBtn.addEventListener("click", function (e) {
         let bodyElem = document.getElementById("nav");
         let alertHtml = `
             <div class="alert alert-info alert-dismissible fade show" role="alert">
-                <strong>Empty Note!</strong> Please insert some text to save the note.
+                <strong>Empty Note Title / Description!</strong> Please insert some text to save the note.
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -37,19 +44,23 @@ addBtn.addEventListener("click", function (e) {
 // Function to show elements from the loal storage
 function showNotes() {
     let notes = localStorage.getItem("notes")
+    let titles = localStorage.getItem("titles")
     if (notes == null) {
         notesObj = [];
+        titlesObj = [];
     }
     else {
         notesObj = JSON.parse(notes);
+        titlesObj = JSON.parse(titles);
     }
 
     let html = "";
     notesObj.forEach(function (element, index) {
+        const titleVal = titlesObj[index];
         html += `
             <div class="noteCard mx-2 my-2" style="width: 18rem;">
                 <div class="card-body">
-                    <h5 class="card-title">Note ${index + 1}</h5>
+                    <h5 class="card-title">${titleVal}</h5>
                     <p class="card-text">${element}</p>
                     <button id="${index}"  onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                 </div>
@@ -68,14 +79,19 @@ function showNotes() {
 function deleteNote(index) {
     //console.log("I am deleting", index);
     let notes = localStorage.getItem("notes")
+    let titles = localStorage.getItem("titles")
     if (notes == null) {
         notesObj = [];
+        itemsObj = [];
     }
     else {
         notesObj = JSON.parse(notes);
+        itemsObj = JSON.parse(titles);
     }
     notesObj.splice(index, 1);
+    itemsObj.splice(index, 1);
     localStorage.setItem("notes", JSON.stringify(notesObj));
+    localStorage.setItem("items", JSON.stringify(itemsObj));
     showNotes();
 }
 
